428097112ce3e0b5e1fa88e6d8f2ca8c273b5e82
Evan Jiang <evanjiangrd@gmail.com>
2025-12-10 12:18:14 -0500

    fix

--- train_dpo.sh
+++ train_dpo.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=80G
-#SBATCH --time=8:00:00
+#SBATCH --time=12:00:00
 #SBATCH --job-name=dpo-toxic
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
